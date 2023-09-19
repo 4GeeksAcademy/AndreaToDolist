a61e39703d7867ef3248217bc6293fd2f9c0677c
Drea Q <marcellab721@icloud.com>
2023-09-19 04:56:28 +0000

    ToDoList

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
+
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setToDos] = useState([]);
+
+  // Add into array -> concat
+  // Delete from array -> filter
+  // Update -> map
+
+  return (
+    <div className="container">
+      <h1>My To Do's</h1>
+      <ul className="card list-group">
+        <li className="list-group-item">
+          <input
+            type="text"
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
+            onKeyUp={(e) => {
+              if (e.key == "Enter") {
+                setToDos([...todos, inputValue]);
+                setInputValue("");
+              }
+            }}
+            placeholder="What Do You Need To Do?"
+          ></input>
+        </li>
+
+        {todos.map((item, index) => (
+          <li className="list-group-item">
+            {item}{" "}
+            <i
+              class="fas fa-trash-alt "
+              onClick={() =>
+                setToDos(
+                  todos.filter((t, currentIndex) => index != currentIndex)
+                )
+              }
+            ></i>
+          </li>
+        ))}
+        <li className="list-group-item">{todos.length} Items Left</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
